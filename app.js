ee3869250cb4555840cf3f36425dd4da7cd3d813
Xin <weijacky78@hotmail.com>
2023-03-21 13:56:22 -0400

    skeleton server

--- app.js
+++ app.js
@@ -4,3 +4,10 @@ const app = express();
 
 const port = 9000;
 
+app.get('/', (req, res) => {
+
+    res.send("hello world");
+
+});
+
+app.listen(port, () => console.log(`App running on port: ${port}`));
